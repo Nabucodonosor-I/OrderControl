f014ee2df6a5e8d2f9ab3e204d741bef4b1ccf2a
HenriquePossato00 <hpossato00@gmail.com>
2024-04-12 17:11:54 -0300

    Design da tela de login e cadastro, e novo nome para o projeto OrderControl

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         // Invoando construtores
-        TelaCadastroLogin sp = new TelaCadastroLogin();
-        // Login login = new Login();
-
-        // login.disposeLogin();                    
+        TelaCadastroLogin sp = new TelaCadastroLogin();                
     }
 }
--- TelaCadastroLogin.java
+++ TelaCadastroLogin.java
@@ -1,64 +1,114 @@
 import javax.swing.*;
-//import javax.swing.border.Border;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TelaCadastroLogin extends JFrame implements ActionListener {
     JFrame framePrincipal = new JFrame();
-    JButton loginButton = new JButton("Login");
-    JButton cadastroButton = new JButton("Cadastrar");  
+    RoundedButton loginButton = new RoundedButton(10, new Color(54, 54, 54));
+    RoundedButton cadastroButton = new RoundedButton(10, new Color(54, 54, 54));
     JButton exitButton = new JButton("x");
     JLabel principalLabel = new JLabel();
-    ImageIcon imageIcon1 = new ImageIcon("68386.png");
-    ImageIcon imageIcon2 = new ImageIcon("provedor-de-internet-2-43-1693542501.png");
+    RoundedPanel painel = new RoundedPanel(20, new Color(28, 35, 43));
+    ImageIcon imageIcon1 = new ImageIcon("Imagens\\68386.png");
+    ImageIcon imageIcon2 = new ImageIcon(
+            new ImageIcon("Imagens\\Logo.png").getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
     // Border border = BorderFactory.createLineBorder(Color.RED,1);
+
+    JTextField usuario = new JTextField();
+    JPasswordField senha = new JPasswordField();
+    JButton submit = new JButton("Enviar ");
+    JLabel label = new JLabel();
+    JLabel senhaLabel = new JLabel();
+    JLabel usuarioLabel = new JLabel();
+    JLabel bemVindo = new JLabel();
+    JLabel credenciais = new JLabel();
+    JLabel orderConttrl = new JLabel();
 
     public TelaCadastroLogin() {
 
         // configurações frame
-        framePrincipal.setSize(500, 500);
-        framePrincipal.setTitle("Mega Fibra OS");
+        framePrincipal.setSize(750, 500);
         framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         framePrincipal.setResizable(false);
         framePrincipal.setUndecorated(true);
         framePrincipal.setVisible(true);
         framePrincipal.setLocationRelativeTo(null);
-        framePrincipal.getContentPane().setBackground(new Color(255, 222, 173));
+        framePrincipal.getContentPane().setBackground(new Color(255, 255, 255));
         framePrincipal.setIconImage(imageIcon1.getImage());
+        
+        // config painel
+        painel.setBounds(15, 15, 350, 470);  
+
+        // config text field usuario
+        usuario.setBounds(470, 175, 200, 30);
+        usuario.setBorder(new BordaCantoArredondado(0, 0, 0, 2, 10));
+
+        // config text field senha
+        senha.setBounds(470, 250, 200, 30);
+        senha.setBorder(new BordaCantoArredondado(0, 0, 0, 2, 10));
+
+        // configurações label senha
+        senhaLabel.setText("Senha ");
+        senhaLabel.setForeground(new Color(0, 0, 0));
+        senhaLabel.setFont(new Font("Arial", Font.BOLD, 15));
+        senhaLabel.setBounds(470, 225, 125, 25);
+
+        // configurações label usuario
+        usuarioLabel.setText("Usuario ");
+        usuarioLabel.setForeground(new Color(0, 0, 0));
+        usuarioLabel.setFont(new Font("Arial", Font.BOLD, 15));
+        usuarioLabel.setBounds(470, 150, 125, 25);
+
+        // onfig Label Bem vindo
+        bemVindo.setText("Bem Vindo");
+        bemVindo.setForeground(new Color(0, 0, 0));
+        bemVindo.setFont(new Font("Arial", Font.BOLD, 25));
+        bemVindo.setBounds(400, 40, 200, 30);
+
+        // config label credenciais
+        credenciais.setText("Insira suas Credenciais, por favor");
+        credenciais.setForeground(new Color(0, 0, 0));
+        credenciais.setFont(new Font("Arial", Font.BOLD, 13));
+        credenciais.setBounds(400, 63, 300, 30);
+
+        // config Label OrderControl
+        orderConttrl.setText("OrderControl");
+        orderConttrl.setForeground(new Color(0, 0, 0));
+        orderConttrl.setFont(new Font("Bauhaus 93", Font.PLAIN, 30));
+        orderConttrl.setBounds(105, 240, 300, 40);
 
         // configuração botao login
-        loginButton.setBounds(175, 250, 150, 50);
+        loginButton.setText("Login");
+        loginButton.setBounds(470, 300, 200, 50);
         loginButton.setFocusable(false);
         loginButton.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 25));
-        loginButton.setForeground(new Color(255, 255, 255));
-        loginButton.setBackground(new Color(54, 54, 54));
+        loginButton.setForeground(new Color(0, 0, 0));
         loginButton.setBorder(BorderFactory.createRaisedBevelBorder());
         loginButton.addActionListener(this);
-        // loginButton.addActionListener(e -> System.out.println("foi"));
 
         // configuração botão cadastro
-        cadastroButton.setBounds(175, 350, 150, 50);
+        cadastroButton.setText("Cadastrar");
+        cadastroButton.setBounds(470, 375, 200, 50);
         cadastroButton.setFocusable(false);
         cadastroButton.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 25));
-        cadastroButton.setForeground(new Color(255, 255, 255));
-        cadastroButton.setBackground(new Color(54, 54, 54));
+        cadastroButton.setForeground(new Color(0, 0, 0));
         cadastroButton.setBorder(BorderFactory.createRaisedBevelBorder());
         cadastroButton.addActionListener(this);
-        // cadastroButton.addActionListener(e -> System.out.println("foi2"));
 
         // onfiguração botão de saida
-        exitButton.setBounds(455, 0, 45, 45);
-        exitButton.setBackground(new Color(0,0,0));
-        exitButton.setFont(new Font("Arial", Font.BOLD, 12));
-        exitButton.setForeground(new Color(255, 255, 255));
+        exitButton.setBounds(705, 0, 45, 45);
+        exitButton.setBackground(new Color(255, 255, 255));
+        exitButton.setFocusable(false);
+        exitButton.setFont(new Font("Arial", Font.BOLD, 20));
+        exitButton.setBorder(BorderFactory.createEmptyBorder());
+        exitButton.setForeground(new Color(0, 0, 0));
         exitButton.addActionListener(this);
 
-
         // configurações label
-        principalLabel.setText("Mega Fibra OS");
-        principalLabel.setBounds(150, 25, 200, 200);
+        principalLabel.setBounds(95, 35, 200, 200);
         principalLabel.setForeground(new Color(255, 0, 0));
         principalLabel.setFont(new Font("MV Boli", Font.BOLD, 25));
         principalLabel.setIcon(imageIcon2);
@@ -67,14 +117,29 @@ public class TelaCadastroLogin extends JFrame implements ActionListener {
         principalLabel.setIconTextGap(-70);
         principalLabel.setVerticalAlignment(JLabel.CENTER);
         principalLabel.setHorizontalAlignment(JLabel.CENTER);
-        // principalLabel.setBorder(border);
 
         // adicionado
         framePrincipal.add(principalLabel);
         framePrincipal.add(loginButton);
         framePrincipal.add(cadastroButton);
         framePrincipal.add(exitButton);
+        framePrincipal.add(usuario);
+        framePrincipal.add(senha);
+        framePrincipal.add(senhaLabel);
+        framePrincipal.add(usuarioLabel);
+        framePrincipal.add(bemVindo);
+        framePrincipal.add(credenciais);
+        framePrincipal.add(orderConttrl);
+        framePrincipal.add(painel);
         framePrincipal.setLayout(null);
+    }
+
+    public String getSenha() {
+        return senha.getText();
+    }
+
+    public String getUsuario() {
+        return usuario.getText();
     }
 
     // acionamento do botão login
@@ -84,8 +149,8 @@ public class TelaCadastroLogin extends JFrame implements ActionListener {
         Object o = e.getSource();
 
         if (o == loginButton) {
-            framePrincipal.dispose();
-            Login login = new Login();
+            System.out.println(getUsuario());
+            System.out.println(getSenha());
         } else if (o == cadastroButton) {
             framePrincipal.dispose();
             Cadastro cadastro = new Cadastro();
@@ -94,4 +159,3 @@ public class TelaCadastroLogin extends JFrame implements ActionListener {
         }
     }
 }
-    
